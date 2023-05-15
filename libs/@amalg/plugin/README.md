b9a5d7f1b00f25df4e48042a74505cf00c2c0be4
cpd <cpd@duck.com>
2023-05-15 17:20:10 -0400

    ci: creates plugin and add first generator

---
+++ libs/@amalg/plugin/README.md
@@ -0,0 +1,11 @@
+# @amalg-plugin
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build @amalg-plugin` to build the library.
+
+## Running unit tests
+
+Run `nx test @amalg-plugin` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/plugin/src/generators/library/generator.ts
@@ -0,0 +1,33 @@
+import installVersioning from '@jscutlery/semver/src/generators/install';
+import installPublishing from 'ngx-deploy-npm/src/generators/install/generator';
+
+import { formatFiles, readNxJson, Tree } from '@nx/devkit';
+import { libraryGenerator } from '@nx/js';
+
+import { LibraryGeneratorSchema } from './schema';
+
+export default async function library(
+  tree: Tree,
+  options: LibraryGeneratorSchema
+) {
+  const workspaceConfigs = readNxJson(tree);
+
+  await libraryGenerator(tree, {
+    name: options.name,
+    importPath: `@${workspaceConfigs.npmScope}/${options.name}`,
+    publishable: true,
+  });
+
+  await installVersioning(tree, {
+    projects: [options.name],
+    enforceConventionalCommits: true,
+    preset: 'conventional',
+    syncVersions: false,
+  });
+
+  await installPublishing(tree, {
+    projects: [options.name],
+  });
+
+  await formatFiles(tree);
+}
